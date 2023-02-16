24e149a0d1e5a36696592ae1a28748c2ae8d07d4
Abdallah Nagi <abdallahmanagi@gmail.com>
2023-02-16 13:51:06 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"programming is like building a mulilingual puzzle);
-
+	puts("\"programmimng is like building a multilingual
+puzzle");
 	return (0);
-
 }
